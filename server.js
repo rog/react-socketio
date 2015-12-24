aedb00b228d1f42c7d878d46b2b92c189153288e
Rogelio Alberto <i@rog.mx>
2015-12-24 03:37:25 -0600

    Removing disconnected audience members

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const moment = require('moment');
+const _ = require('lodash');
 const debug = require('debug')('server');
 
 const app = express();
@@ -15,6 +16,12 @@ app.use(express.static('./node_modules/bootstrap/dist'));
 
 io.sockets.on('connection', function onConnect(socket) {
   socket.once('disconnect', function onDisconnect() {
+    const member = _.findWhere(audience, { id: this.id });
+    if (member) {
+      audience.splice(audience.indexOf(member), 1);
+      io.sockets.emit('audience', audience);
+      debug(`Left: ${member.name} (Total: ${audience.length})`);
+    }
     connections.splice(connections.indexOf(socket), 1);
     socket.disconnect();
     debug(`Disconnected: ${connections.length} sockets`);
